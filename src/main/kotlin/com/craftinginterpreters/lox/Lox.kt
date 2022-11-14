511136ca6adbe14a0a3b8caf59766407073c4f70
Michael Valladolid <mikevalladolid@gmail.com>
2022-11-14 09:21:15 +0900

    working scanner

--- src/main/kotlin/com/craftinginterpreters/lox/Lox.kt
+++ src/main/kotlin/com/craftinginterpreters/lox/Lox.kt
@@ -1,9 +1,9 @@
+import com.craftinginterpreters.lox.Scanner
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.nio.charset.Charset
 import java.nio.file.Files
 import java.nio.file.Paths
-import java.util.Scanner
 import kotlin.system.exitProcess
 
 class Lox {
@@ -46,7 +46,7 @@ class Lox {
 
         private fun run(source: String) {
             val scanner = Scanner(source)
-            val tokens = scanner.tokens()
+            val tokens = scanner.scanTokens()
 
             for (token in tokens) {
                 println(token)
--- src/main/kotlin/com/craftinginterpreters/lox/Scanner.kt
+++ src/main/kotlin/com/craftinginterpreters/lox/Scanner.kt
@@ -10,12 +10,159 @@ data class Scanner(val source: String) {
     fun scanTokens(): List<Token> {
         while (!isAtEnd()) {
             start = current
-//            scanToken()
+            scanToken()
         }
 
         tokens.add(Token(TokenType.EOF, "", null, line))
         return tokens
     }
 
-    fun isAtEnd(): Boolean = current >= source.length
+    private fun scanToken() {
+        when (val c = advance()) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+
+            '!' -> addToken(if (match('=')) TokenType.BANG_EQUAL else TokenType.BANG)
+            '=' -> addToken(if (match('=')) TokenType.EQUAL_EQUAL else TokenType.EQUAL)
+            '<' -> addToken(if (match('=')) TokenType.LESS_EQUAL else TokenType.LESS)
+            '>' -> addToken(if (match('=')) TokenType.GREATER_EQUAL else TokenType.GREATER)
+
+            '/' -> if (match('/')) {
+                while (peek() != '\n' && !isAtEnd()) advance()
+            } else {
+                addToken(TokenType.SLASH)
+            }
+
+            // whitespaces
+            ' ', '\r', '\t' -> {}
+            '\n' -> line++
+
+            // strings
+            '"' -> string()
+
+            else -> {
+                if (isDigit(c)) {
+                    number()
+                } else if (isAlpha(c)) {
+                    identifier()
+                } else {
+                    Lox.error(line, "Unexpected character. $c")
+                }
+            }
+        }
+    }
+
+    private fun isAtEnd(): Boolean = current >= source.length
+
+    private fun addToken(type: TokenType) {
+        addToken(type, null)
+    }
+
+    private fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+
+    private fun advance(): Char {
+        return source[current++]
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+
+        current++
+        return true
+    }
+
+    private fun peek(): Char {
+        if (isAtEnd()) return Char.MIN_VALUE
+        return source[current]
+    }
+
+    private fun peekNext(): Char {
+        if (current + 1 >= source.length) return Char.MIN_VALUE
+        return source[current + 1]
+    }
+
+    private fun string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n') line++
+            advance()
+        }
+
+        if (isAtEnd()) {
+            Lox.error(line, "Unterminated string.")
+            return
+        }
+
+        advance()
+
+        val value = source.substring(start + 1, current - 1)
+        addToken(TokenType.STRING, value)
+    }
+
+    private fun isDigit(c: Char): Boolean {
+        return c >= '0' && c <= '9'
+    }
+
+    private fun isAlpha(c: Char): Boolean {
+        return (c in 'a'..'z') ||
+                (c in 'B'..'Z') ||
+                c == '_'
+    }
+
+    private fun isAlphaNumeric(c: Char): Boolean {
+        return isAlpha(c) || isDigit(c)
+    }
+
+    private fun number() {
+        while (isDigit(peek())) advance()
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance()
+
+            while (isDigit(peek())) advance()
+        }
+
+        addToken(TokenType.NUMBER, source.substring(start, current).toDouble())
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek())) advance()
+
+        val text = source.substring(start, current)
+        val type = keywords[text] ?: TokenType.IDENTIFIER
+
+        addToken(type)
+    }
+
+    companion object {
+        private val keywords = mapOf(
+            "and" to TokenType.AND,
+            "class" to TokenType.CLASS,
+            "else" to TokenType.ELSE,
+            "false" to TokenType.FALSE,
+            "for" to TokenType.FOR,
+            "fun" to TokenType.FUN,
+            "if" to TokenType.IF,
+            "nil" to TokenType.NIL,
+            "or" to TokenType.OR,
+            "print" to TokenType.PRINT,
+            "return" to TokenType.RETURN,
+            "super" to TokenType.SUPER,
+            "this" to TokenType.THIS,
+            "true" to TokenType.TRUE,
+            "var" to TokenType.VAR,
+            "while" to TokenType.WHILE,
+        )
+    }
 }
